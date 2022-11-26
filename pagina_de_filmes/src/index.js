e0e88c70c9b7a0557c037496bf2dd45b61431cfb
alan lima <alanlima4123@gmail.com>
2022-11-26 19:33:08 -0300

    Adicionando github.io

--- pagina_de_filmes/src/index.js
+++ pagina_de_filmes/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Routes from './Routes';
+import Routes from './Routes/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Routes />
+      <Routes/>
   </React.StrictMode>
 );
 
